72464ede8b0c19799665bedf63ceac39f2ee21e7
Nafeeur <inafeeur@gmail.com>
2022-09-28 20:05:39 +0600

    Sign Up design Complete

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,21 @@
-import ApplicationLogo from './components/ApplicationLogo'
-import AuthCard from './components/AuthCard'
-import Button from './components/Button'
-import GuestLayout from './components/Layouts/GuestLayout'
-import Input from './components/Input'
-import InputError from './components/InputError'
-import Label from './components/Label'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { Popover, Transition } from '@headlessui/react'
+import {
+    Bars3Icon,
+    XMarkIcon,
+    ArrowPathIcon,
+    BookmarkSquareIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    CursorArrowRaysIcon,
+    LifebuoyIcon,
+    PhoneIcon,
+    PlayIcon,
+    ShieldCheckIcon,
+    Squares2X2Icon,
+} from '@heroicons/react/24/outline'
 
 const Register = () => {
 
@@ -16,102 +25,189 @@ const Register = () => {
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
     const [errors, setErrors] = useState([])
 
-    const submitForm = event => {
-        event.preventDefault()
+    // const submitForm = event => {
+    //     event.preventDefault()
 
-        // register({ name, email, password, password_confirmation: passwordConfirmation, setErrors })
-    }
+    //     // register({ name, email, password, password_confirmation: passwordConfirmation, setErrors })
+    // }
 
     return (
-        <GuestLayout>
-            <AuthCard
-                logo={
-                    <Link href="/">
-                        <a>
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                        </a>
-                    </Link>
-                }>
+        <div>
+            <Popover className="relative bg-white">
+                <div className="mx-auto px-4 sm:px-6">
+                    <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                        <div className="flex justify-start lg:w-0 lg:flex-1">
+                            <Link href="/">
+                                <div>
+                                    <span className="sr-only">Your Company</span>
+                                    <img
+                                        className="h-8 w-auto sm:h-10"
+                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                        alt=""
+                                    />
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="-my-2 -mr-2 md:hidden">
+                            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                <span className="sr-only">Open menu</span>
+                                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                            </Popover.Button>
+                        </div>
+                        <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+                        </Popover.Group>
+                        <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                                Sign in
+                            </a>
+                            <Link
+                                href="/register"
+                                className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                            >
+                                Sign up
+                            </Link>
+                        </div>
+                    </div>
+                </div>
 
-                <form onSubmit={}>
-                    {/* Name */}
+                <Transition
+                    as={Fragment}
+                    enter="duration-200 ease-out"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="duration-100 ease-in"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+                        <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                            <div className="px-5 pt-5 pb-6">
+                                <div className="flex items-center justify-between">
+                                    <div>
+                                        <img
+                                            className="h-8 w-auto"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                            alt="Your Company"
+                                        />
+                                    </div>
+                                    <div className="-mr-2">
+                                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                            <span className="sr-only">Close menu</span>
+                                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                        </Popover.Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="space-y-6 py-6 px-5">
+                                <div>
+                                    <Link href="/register"
+                                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                                    >
+                                        Sign up
+                                    </Link>
+                                    <p className="mt-6 text-center text-base font-medium text-gray-500">
+                                        Existing customer?{' '}
+                                        <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                                            Sign in
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Popover.Panel>
+                </Transition>
+            </Popover>
+
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
                     <div>
-                        <Label htmlFor="name" className={''}>Name</Label>
-
-                        <Input
-                            id="name"
-                            type="text"
-                            value={name}
-                            className="block mt-1 w-full"
-                            onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                const element = event.currentTarget as HTMLInputElement
-                                const value = element.value
-                                setName(value)
-                            }}
-                            required
-                            autoFocus
+                        <img
+                            className="mx-auto h-12 w-auto"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            alt="Your Company"
                         />
+                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                            Sign Up
+                        </h2>
                     </div>
+                    <form className="mt-8 space-y-6" action="#" method="POST">
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="-space-y-px rounded-md shadow-sm">
 
-                    {/* Email Address */}
-                    <div className="mt-4">
-                        <Label htmlFor="email" className={''}>Email</Label>
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Full Name
+                                </label>
+                                <input
+                                    id="full-name"
+                                    name="name"
+                                    type="name"
+                                    autoComplete="name"
+                                    required
+                                    className="mb-3 relative block w-full appearance-none rounded border border-gray-300 px-3 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Full Name"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="mb-3 relative block w-full appearance-none rounded border border-gray-300 px-3 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="mb-3 relative block w-full appearance-none rounded border border-gray-300 px-3 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
 
-                        <Input
-                            id="email"
-                            type="email"
-                            value={email}
-                            className="block mt-1 w-full"
-                            onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                const element = event.currentTarget as HTMLInputElement
-                                const value = element.value
-                                setEmail(value)
-                            }}
-                            required
-                        />
-                    </div>
+                            <div>
+                                <label htmlFor="password_confirmation" className="sr-only">
+                                    Password Confirmation
+                                </label>
+                                <input
+                                    id="password-confirmation"
+                                    name="password_confirmation"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="mb-3 relative block w-full appearance-none rounded border border-gray-300 px-3 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Password Confirmation"
+                                />
+                            </div>
+                        </div>
 
-                    {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password" className={''}>Password</Label>
-
-                        <Input
-                            id="password"
-                            type="password"
-                            value={password}
-                            className="block mt-1 w-full"
-                            onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                const element = event.currentTarget as HTMLInputElement
-                                const value = element.value
-                                setPassword(value)
-                            }}
-                            required
-                            autoComplete="new-password"
-                        />
-                    </div>
-
-                    {/* Confirm Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="passwordConfirmation" className={''}>
-                            Confirm Password
-                        </Label>
-
-                        <Input
-                            id="passwordConfirmation"
-                            type="password"
-                            value={passwordConfirmation}
-                            className="block mt-1 w-full"
-                            onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                const element = event.currentTarget as HTMLInputElement
-                                const value = element.value
-                                setPasswordConfirmation(value)
-                            }}
-                            required
-                        />
-                    </div>
-                </form>
-            </AuthCard>
-        </GuestLayout>
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                </span>
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
